ccec06104525b04398c7ac1d9729119a9ee127bd
Naohiro Sakuma <nao.bear@gmail.com>
2014-02-11 03:06:07 +0900

    bootup rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WatsonTest::Application.config.secret_key_base = 'ffb8b30fdd136cba817cfddad8c4c166aba3620debc33b5903a6c81f03ca38b30785bf85bcb62ac6967c582d5c1e756005d3a9fa5b91d5bceca4947c730f4a8b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WatsonTest::Application.config.session_store :cookie_store, key: '_watson-test_session'
